712b8691d688bd45676e47e119bf1f2587d88a2b
Anik Ghosh <anik.ghosh@rwth-aachen.de>
2023-04-12 08:54:46 +0200

    Modified Modal Component and Founders Page

--- src/components/Founders.jsx
+++ src/components/Founders.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import PageSection from './PageSection';
 
 import rafael from '../assets/team/rafael.jpg';
 import rebekka from '../assets/team/rebekka.jpg';
+import sikander from '../assets/team/sikander.jpg';
+import konrad from '../assets/team/konrad.jpg';
+import Modal from './Modal';
 
 const Founders = () => {
+
+    const [showModal, setShowModal] = useState(false);
+    const [urlString, setUrlString] = useState('');
+
+    const handleOnOpen = (urlString) => {
+        setShowModal(true)
+        setUrlString(urlString)
+    }
+
+    const handleOnClose = () => setShowModal(false)
+
   return (
     <PageSection 
     name="founders"
     title="Founders"
     subtitle={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie tristique feugiat. Curabitur tincidunt ex sed velit tempus, rhoncus porta lacus bibendum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sit amet turpis id augue aliquet iaculis at sit amet dui. Aenean sollicitudin diam lobortis, pharetra urna at, ultricies turpis. Phasellus vitae aliquam lorem, et congue quam. Vestibulum bibendum justo odio, id placerat dolor euismod at. Etiam ultricies ipsum vitae tellus tincidunt, eget posuere enim ultricies. Nam ultricies nisi ultricies urna luctus sodales. Pellentesque ornare iaculis varius. Nulla sit amet ante turpis. Vestibulum vel lorem at arcu eleifend vestibulum a vel odio. Proin quis luctus ante. Fusce a luctus mi.`}
     >
-        <div className="grid grid-cols-2 gap-4">
+        {/* <div className="grid grid-cols-2 gap-4">
             <div className="rounded-xl border-4  h-full px-4 py-4 flex">
                 <div className="w-1/3">
                     <img className="h-72 rounded-xl" src={rafael} alt="" />
@@ -32,7 +46,41 @@ const Founders = () => {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie tristique feugiat. Curabitur tincidunt ex sed velit tempus, rhoncus porta lacus bibendum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sit amet turpis id augue aliquet iaculis at sit amet dui. Aenean sollicitudin diam lobortis, pharetra urna at, ultricies turpis. Phasellus vitae aliquam lorem, et congue quam.</p>
                 </div>
             </div>
+        </div> */}
+
+        <div class="flex flex-wrap gap-0">
+            <div onClick={() => handleOnOpen(rafael)}
+            class="relative w-1/2 md:w-1/4 lg:w-1/4 text-center hover:transform hover:scale-125 hover:rounded transition hover:z-50 hover:cursor-pointer duration-50 ease-in-out">
+                <img src={rafael} alt="Rafael Kramann" />
+                <div class="absolute bottom-0 left-0 right-0 px-2 py-2 bg-black bg-opacity-50 text-white">
+                    <h5>Rafael Kramann</h5>
+                </div>
+            </div>
+            <div onClick={() => handleOnOpen(rebekka)}
+            class="relative w-1/2 md:w-1/4 lg:w-1/4 text-center hover:transform hover:scale-125 hover:rounded transition hover:z-50 hover:cursor-pointer duration-50 ease-in-out">
+                <img src={rebekka} alt="Rebekka Kramann-Schneider" />
+                <div class="absolute bottom-0 left-0 right-0 px-2 py-2 bg-black bg-opacity-50 text-white">
+                    <h5>Rebekka Schneider-Kramann</h5>
+                </div>
+            </div>
+            <div onClick={() => handleOnOpen(sikander)}
+            class="relative w-1/2 md:w-1/4 lg:w-1/4 text-center hover:transform hover:scale-125 hover:rounded transition hover:z-50 hover:cursor-pointer duration-50 ease-in-out">
+                <img src={sikander} alt="Sikander Hayat" />
+                <div class="absolute bottom-0 left-0 right-0 px-2 py-2 bg-black bg-opacity-50 text-white">
+                    <h5>Sikander Hayat</h5>
+                </div>
+            </div>
+            <div onClick={() => handleOnOpen(konrad)}
+            class="relative w-1/2 md:w-1/4 lg:w-1/4 text-center hover:transform hover:scale-125 hover:rounded transition hover:z-50 hover:cursor-pointer duration-50 ease-in-out">
+                <img src={konrad} alt="Konrad Hoeft" />
+                <div class="absolute bottom-0 left-0 right-0 px-2 py-2 bg-black bg-opacity-50 text-white">
+                    <h5>Konrad Hoeft</h5>
+                </div>
+            </div>
         </div>
+        <Modal onClose={handleOnClose} visible={showModal} imgUrl={urlString}/>
+
+
     </PageSection>
   )
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-export default function Modal({visible, onClose}) {
+export default function Modal({visible, onClose, imgUrl}) {
+
+
+    const handleOnClose = (e) => {
+        if(e.target.id === "modal-body") onClose()
+    }
 
     if (!visible) return null;
 
     return (
-        <div onClick={() => onClose()} className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
+        <div 
+        id = "modal-body"
+        onClick={handleOnClose} 
+        className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
             <div className="w-[600px]">
                 <div className="bg-white p-2 rounded">
-                    Details Page. Needs work.
+                    <img className="w-48 h-96" src={imgUrl} alt={imgUrl}/>
                 </div>
             </div>
         </div>
